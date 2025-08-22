09954926fc185da7f2b27d6a2777282fda53959a
phpontes <ppontes95@hotmail.com>
2025-08-22 12:12:02 -0300

    getting started with a custom directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component, computed, inject } from '@angular/core';
-import { NgIf } from '@angular/common';
 
 import { AuthComponent } from './auth/auth.component';
-import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, NgIf],
+  imports: [AuthComponent, LearningResourcesComponent],
 })
 export class AppComponent {
   private authService = inject(AuthService);
