b6649cea037b956069733875ce7362a6baf5e9f6
phpontes <ppontes95@hotmail.com>
2025-08-22 12:23:24 -0300

    finished appSafeLink directive to enhance link safety and user confirmation

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -3,9 +3,19 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('SafeLinkDirective initialized');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you really want to leave the app?');
+    if (!wantsToLeave) {
+      event.preventDefault();
+    }
   }
 }
